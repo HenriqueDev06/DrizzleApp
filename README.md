9bed6ec1882c5c6b88fb89e27cb67e7da9e7f482
HenriqueDev06 <johanhenrique18@gmail.com>
2022-03-01 01:09:32 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-epmnec
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-epmnec)
---
+++ app.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import GlobalStyles from './styles/globalStyles';
+
+const App = () => {
+  return (
+    <Fragment>
+      <GlobalStyles />
+    </Fragment>
+  );
+};
+
+export default App;
